ce5e7d412f90fc72b3bdf2a089c4735b67850bd5
unknown <Amit1paz@gmail.com>
2022-04-15 18:15:37 +0300

    cdc-ds: publish

---
+++ rollup.config.mjs
@@ -0,0 +1,24 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import PeerDepsExternalPlugin from 'rollup-plugin-peer-deps-external';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'cjs'
+  },
+  plugins: [
+    resolve(),
+    babel.babel({
+      babelHelpers: 'bundled',
+      presets: [
+        "@babel/preset-react"
+      ],
+      exclude: 'node_modules/**'
+    }),
+    PeerDepsExternalPlugin(),
+    commonjs()
+  ]
+};
