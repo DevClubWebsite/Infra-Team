98028c3381631e044079e144e81d00b576dfbb7f
SorerBOT <alonfiller@gmail.com>
2022-04-16 16:51:30 +0300

    (Feat) Created a Theme file.

---
+++ src/Theme/Theme.js
@@ -0,0 +1,14 @@
+const Theme = {
+    colours: {
+        blue: {
+            primary: "#21213E",
+            secondary: "#121231",
+            off: "#0A0A1B",
+        },
+        yellow: {
+            primary: "#F6C927"
+        }
+    }
+}
+
+export default Theme;
