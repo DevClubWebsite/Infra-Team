6c252c5aa5d25190ad547378a3e7b44a0e1eea84
unknown <Amit1paz@gmail.com>
2022-04-17 19:16:25 +0300

    (Feat) created onclick function to set color

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyledNavbar } from "./Navbar.styled";
 import PropTypes from 'prop-types';
 import NavbarButton from '../NavbarButton/NavbarButton';
-import { v4 as uuidv4 } from "uuid";
 import GlobalStyles from "../../Theme/Global";
 
 function Navbar({ items, direction = 'row', gap = 1 }) {
+    const [focusedElement, setFocusedElement] = useState();
+
+
     return <>
         <GlobalStyles />
         <StyledNavbar direction={direction} gap={gap}>
             <ul>
-                {items.map(item => {
+                {items.map((item, index) => {
                     return (
-                        <li key={uuidv4()}>
-                            <NavbarButton label={item.label} url={item.url}/>
+                        <li key={index}>
+                            <NavbarButton
+                            label={item.label}
+                            url={item.url}
+                            focusedElement={focusedElement}
+                            setFocusedElement={setFocusedElement}
+                            id={index}
+                            />
                         </li>
                     )
                 })}
