aaeb3e0c7a039af8bdff22a0a2b0e11aa39762f9
SorerBOT <alonfiller@gmail.com>
2022-04-17 16:32:46 +0300

    (Feat) Created HeaderParagraph Component, a Component which unites a Header and a Pargraph, and scales them appropriately.

---
+++ src/components/HeaderParagraph/HeaderParagraph.stories.js
@@ -0,0 +1,19 @@
+import React from "react";
+import HeaderParagraph from "./HeaderParagraph";
+import Theme from "../../Theme/Theme";
+
+export default {
+    title: "HeaderParagraph",
+    component: HeaderParagraph
+}
+
+const Template = (args) => <HeaderParagraph {...args} />;
+export const Primary = Template.bind({});
+
+Primary.args = {
+    size: 1,
+    headerLabel: "Who loves Minecraft?",
+    paragraphLabel: "I DO!",
+    headerColour: Theme.colours.yellow.primary,
+    paragraphColour: Theme.colours.blue.off
+}
