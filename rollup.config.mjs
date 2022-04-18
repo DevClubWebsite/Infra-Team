5a6625f0ed6326a86c23293dc7b006e3bd89afbb
ozgemer-git <oz_gemer@outlook.com>
2022-04-18 11:57:52 +0300

    jsx plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,24 +1,24 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
-import PeerDepsExternalPlugin from 'rollup-plugin-peer-deps-external';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import babel from "@rollup/plugin-babel";
+import jsx from "@babel/plugin-syntax-jsx";
+import PeerDepsExternalPlugin from "rollup-plugin-peer-deps-external";
 
 export default {
-  input: 'src/index.js',
+  input: "src/index.js",
   output: {
-    file: 'dist/bundle.js',
-    format: 'cjs'
+    file: "dist/bundle.js",
+    format: "cjs",
   },
   plugins: [
     resolve(),
     babel.babel({
-      babelHelpers: 'bundled',
-      presets: [
-        "@babel/preset-react"
-      ],
-      exclude: 'node_modules/**'
+      babelHelpers: "bundled",
+      presets: ["@babel/preset-react"],
+      exclude: "node_modules/**",
     }),
     PeerDepsExternalPlugin(),
-    commonjs()
-  ]
+    jsx(),
+    commonjs(),
+  ],
 };
