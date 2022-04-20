9d0cdb9a03e9379df4acbf1174e26149da4a6ace
unknown <Amit1paz@gmail.com>
2022-04-20 21:28:46 +0300

    (Patch) ButtonCTA - add Typography

--- src/components/ButtonCTA/ButtonCTA.js
+++ src/components/ButtonCTA/ButtonCTA.js
@@ -3,13 +3,14 @@ import PropTypes from "prop-types";
 import { ArrowDiv, StyledButtonCTA } from "./ButtonCTA.styled";
 import GlobalStyles from "../../Theme/Global";
 import Arrow from "../../Images/yellow-arrow-icon.svg";
+import Typography from "../Typography/Typography";
 
 function ButtonCTA({ label, size = "regular" }) {
   return (
     <>
       <GlobalStyles />
       <StyledButtonCTA size={size}>
-        <p>{label}</p>
+        <Typography variant="p" color="black" label={label} />
         <ArrowDiv size={size}>
           <img src={Arrow} alt="arrow" />
         </ArrowDiv>
