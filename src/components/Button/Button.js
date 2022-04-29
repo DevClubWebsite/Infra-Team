bde65c85dfae34c68a5e6697aa1ed2a529c2d2c2
SorerBOT <alonfiller@gmail.com>
2022-04-29 19:38:46 +0300

    (Patch) Enabled Button to receive an onclick event as a parameter

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { Children } from "react";
 import PropTypes from "prop-types";
 import { StyledButton } from "./Button.styled";
 import GlobalStyles from "../../Theme/Global";
 import Theme from "../../Theme/Theme";
-
-export default function Button({ background = Theme.background.dark, color = "none", variant = "regular", width, height, padding = 0.2 }) {
+import Typography from "../Typography/Typography";
+export default function Button({ background = Theme.background.dark, color = "none", variant = "regular", width, height, padding = 0.2, onclick }) {
   return (
     <>
       <GlobalStyles />
-      <StyledButton background={background} color={color} variant={variant} width={width} height={height} padding={padding} />
+      <StyledButton onClick={() => onclick()} background={background} color={color} variant={variant} width={width} height={height} padding={padding}>
+      </StyledButton>
     </>
   );
 }
@@ -19,5 +20,7 @@ Button.propTypes = {
   variant: PropTypes.oneOf(["regular", "outlined", "text"]),
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
-  padding: PropTypes.number
+  padding: PropTypes.number,
+  onclick: PropTypes.func,
+  component: PropTypes.any
 };
---
+++ src/components/Dialog/Dialog.styled.js
@@ -0,0 +1,21 @@
+import { Dialog } from "@headlessui/react";
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+    width: ${({ width }) => width}rem;
+    height: ${({ height }) => height}rem;
+    background-color: ${({ background }) => background};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const StyledDialog = (variant) => {
+    if (variant === Dialog.Title) {
+        return styled(variant)`
+            color: ${({ titlecolor }) => titlecolor};
+        
+        
+        `;
+    }
+}
