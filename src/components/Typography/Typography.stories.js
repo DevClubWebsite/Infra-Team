5c61ec1f9398145c69cbc26794b41a1cbf69ba4c
unknown <Amit1paz@gmail.com>
2022-05-01 21:24:34 +0300

    (Patch) Typograpghy- storybook contorls -> select

--- src/components/Typography/Typography.stories.js
+++ src/components/Typography/Typography.stories.js
@@ -2,7 +2,13 @@ import Typography from "./Typography";
 
 export default {
     title: "Text/text",
-    component: Typography
+    component: Typography,
+    argTypes: {
+        as: {
+            options: ["p", "h1", "h2", "h3", "h4", "h5", "h6"],
+            control: { type: 'select' }
+        }
+    }
 }
 
 const Template = (args) => <Typography {...args} />;
