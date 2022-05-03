7764408f38c65f6887e50e9549720cbb5bdf7d05
ozgemer-git <oz_gemer@outlook.com>
2022-05-03 14:52:44 +0300

    storybook restructure

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -1,7 +1,7 @@
 import styled, { css } from "styled-components";
 
 export const StyledButton = styled.button`
-  padding: 0.2rem;
+  padding: 0.5rem;
   min-width: ${({ width }) => width}rem;
   min-height: ${({ height }) => height}rem;
   border-radius: 0.277rem;
@@ -12,6 +12,5 @@ export const StyledButton = styled.button`
     as === "outlined" &&
     css`
       border: 1.5px solid ${({ color }) => color};
-    `
-  }
+    `}
 `;
