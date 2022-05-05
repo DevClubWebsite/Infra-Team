cbf21cede9f00a4032d2d863c44c362d2cb108aa
unknown <Amit1paz@gmail.com>
2022-05-05 19:46:14 +0300

    (Feat) menu v1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import Button from './components/Button/Button';
-import ButtonCTA from './components/ButtonCTA/ButtonCTA';
-import Navbar from './components/Navbar/Navbar';
-import Theme from './Theme/Theme';
-import Typography from './components/Typography/Typography';
-import Card from './components/Card/Card';
-import GlobalStyles from './Theme/Global';
-import Tabs from './components/Tabs/Tabs';
+import Button from "./components/Button/Button";
+import ButtonCTA from "./components/ButtonCTA/ButtonCTA";
+import Navbar from "./components/Navbar/Navbar";
+import Theme from "./Theme/Theme";
+import Typography from "./components/Typography/Typography";
+import Card from "./components/Card/Card";
+import GlobalStyles from "./Theme/Global";
+import Tabs from "./components/Tabs/Tabs";
 
 export {
   Button,
@@ -15,5 +15,5 @@ export {
   Card,
   Tabs,
   Theme,
-  GlobalStyles
-}
+  GlobalStyles,
+};
